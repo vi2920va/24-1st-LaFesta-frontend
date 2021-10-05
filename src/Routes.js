5474c94a755e7a876ad11494e7abf8e9f7f299a8
vi2920va <vi2920va@gmail.com>
2021-10-05 22:26:59 +0900

    Add: 프로젝트 초기세팅

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Main from './pages/Main/Main';
+import Nav from './components/Nav/Nav';
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Main} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+export default Routes;
