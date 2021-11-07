b64188b3ccbcb58ab076267166ffb9a136948a23
vi2920va <vi2920va@gmail.com>
2021-11-07 22:03:49 +0900

    Refactor : Main Products Slide - innerWidth가 변화할 때마다 자동으로  slideSize 사이즈가 변화하다록 로직 구현 - innerWidth가 변화할 때마다 움직임이 부드럽지 않은 점 개선 요망

--- src/pages/Main/Products/Products.jsx
+++ src/pages/Main/Products/Products.jsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Slide from '../../../components/Slide/Slide';
 import ProductsItem from './ProductsItem/ProductsItem';
 import './Products.scss';
 
 function Products() {
+
+  const slideRef = useRef();
   const [count, setCount] = useState(1);
   const [slideSize, setSlideSize] = useState(0);
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
-  const [largePercent, setLargePercent] = useState(482);
-  const [mediumPercent, setMediumPercent] = useState(333);
-  const [smallPercent, setSmallPercent] = useState(615);
   const [productList, setProductList] = useState([]);
-
 
   useEffect(() => {
     fetch('/data/mockData.json')
@@ -19,44 +17,32 @@ function Products() {
       .then(data => setProductList(data.products));
   }, []);
 
-  const onWindowSize = useCallback(() => {
-    setInnerWidth(window.innerWidth);
-  }, [innerWidth]);
-
-
-
+  
   useEffect(() => {
+    const onWindowSize = () => {
+      setInnerWidth(window.innerWidth);
+    };
     window.addEventListener('resize', onWindowSize);
-
-
-    if (innerWidth >= 1190 && count > 1) {
-      console.log('all', 'w :', innerWidth, 'c :', count);
-      const size = Math.round((innerWidth / 3.3) * (count - 1) * -1);
-      console.log(size);
-    }
 
     return () => {
       window.addEventListener('remove', onWindowSize);
     };
-  }, [innerWidth, count]);
+  }, []); 
 
   useEffect(() => {
-
-  }, [innerWidth, count]);
+    if(slideRef.current){
+      setSlideSize((slideRef.current.children[0].offsetWidth + 1) * (count - 1) * -1);
+    }
+  }, [count, innerWidth]);
 
 
   const handleSlide = (type, slideCount) => {
-
     if (type === "next") {
-
-      setSlideSize(innerWidth >= 1190 ? largePercent * count * -1 : innerWidth <= 1190 && innerWidth >= 768 ? mediumPercent * count * -1 : smallPercent * count * -1);
       setCount(slideCount + 1);
     } else {
-      setSlideSize(innerWidth >= 1190 ? slideSize + largePercent : innerWidth <= 1190 && innerWidth >= 768 ? slideSize + mediumPercent : slideSize + smallPercent);
       setCount(slideCount - 1);
     }
   };
-
   return (
     <>
       <Slide
@@ -66,7 +52,8 @@ function Products() {
         onSlide={handleSlide}
       >
         <ul
-          className="products__image-list" id="products__image-list"
+          className="products__image-list"
+          ref={slideRef}
           style={{ transform: `translateX(${slideSize}px)` }}
         >
           {productList.map(item => <ProductsItem key={item.id} item={item} />
